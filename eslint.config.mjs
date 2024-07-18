ecd7f4a6247cb7b9ebd22f43b80ee18c3a358dd6
EIji Tachikawa <tsuyoku1202@gmail.com>
2024-07-19 07:22:33 +0900

    eslintの設定 Fixes #6

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import typescriptParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
+
+const myRules = {
+  "@next/next/inline-script-id": "off",
+  "@typescript-eslint/no-unused-vars": "off",
+  "unused-imports/no-unused-imports": "error",
+  "unused-imports/no-unused-vars": [
+    "warn",
+    {
+      vars: "all",
+      varsIgnorePattern: "^_",
+      args: "after-used",
+      argsIgnorePattern: "^_",
+    },
+  ],
+};
+
+export default [
+  {
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        React: "readonly",
+      },
+    },
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+      "@next/next": nextPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@next/next/no-img-element": "error",
+      ...myRules,
+    },
+  },
+  {
+    ignores: [".next/**/*", "node_modules/**/*"],
+  },
+];
