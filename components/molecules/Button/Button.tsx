a800076a4b7bfc486350f3274ddd2be2bae0b2ad
EIji Tachikawa <tsuyoku1202@gmail.com>
2024-07-21 07:35:37 +0900

    feat: Buttonコンポーネントの作成 Fixes #19

---
+++ components/molecules/Button/Button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import clsx from "clsx";
+import s from "./Button.module.sass";
+
+type Props = {
+  label: string;
+  className?: string;
+  disabled?: boolean;
+  type?: "button" | "submit";
+  variant?: "primary" | "secondary" | "outline";
+  size?: "xs" | "sm" | "md" | "lg";
+  isLoading?: boolean;
+  onClick?: () => void;
+};
+
+const Button: React.FC<Props> = ({
+  label,
+  className,
+  disabled = false,
+  type = "button",
+  variant = "primary",
+  size = "md",
+  isLoading = false,
+  onClick,
+}) => {
+  const buttonClassNames = clsx(s.button, className, {
+    [s.primary]: variant === "primary",
+    [s.secondary]: variant === "secondary",
+    [s.outline]: variant === "outline",
+    [s.xs]: size === "xs",
+    [s.sm]: size === "sm",
+    [s.md]: size === "md",
+    [s.lg]: size === "lg",
+    [s.disabled]: disabled || isLoading,
+  });
+  return (
+    <button
+      className={buttonClassNames}
+      disabled={disabled || isLoading}
+      type={type}
+      onClick={onClick}
+    >
+      {isLoading ? "Loading..." : label}
+    </button>
+  );
+};
+
+export default Button;
