8200cf41874ff07daeb11045bba19b67f39e1a8e
EIji Tachikawa <tsuyoku1202@gmail.com>
2024-07-26 14:29:47 +0900

    feat: 新規ユーザー登録の繋ぎ込み Fixes #55
    console削除
    
    fix: console.log削除

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,48 @@
+import { db, auth } from '@/config/firebase';
+import { collection, addDoc, query, where, getDocs } from 'firebase/firestore';
+import { NextResponse, NextRequest } from 'next/server';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import bcrypt from 'bcrypt';
+
+// 新規ユーザー登録
+export async function POST(request: NextRequest) {
+  const requestData = await request.json();
+  try {
+    const { username, email, password, profileIcon, dateOfBirth, gender } = requestData;
+
+    // メールアドレスが既に登録されていないか確認
+    const q = query(collection(db, 'users'), where('email', '==', email));
+    const userSnapshot = await getDocs(q);
+
+    if (!userSnapshot.empty) {
+      return NextResponse.json({ error: 'このメールアドレスは既に使用されています。' }, { status: 400 });
+    }
+
+    // パスワードをハッシュ化
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Firebase Authでユーザー作成
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // Firestoreにユーザーデータを保存
+    await addDoc(collection(db, 'users'), {
+      userId: user.uid,
+      username,
+      email,
+      password: hashedPassword,
+      profileIcon,
+      dateOfBirth: {
+        year: dateOfBirth.year,
+        month: dateOfBirth.month,
+        day: dateOfBirth.day,
+      },
+      gender,
+      createdAt: new Date()
+    });
+
+    return NextResponse.json({ message: '新規ユーザー登録に成功しました。' });
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
+  }
+}
